2986f447a09acf288ce2e7d8fc9b2dddb990acbd
Dan Heberden <danheberden@gmail.com>
2012-12-21 12:22:43 -0800

    update to using gith, add tests and troubleshoot problems

--- test/gdt_test.js
+++ test/gdt_test.js
@@ -23,13 +23,62 @@ var gdt = require('../lib/gdt.js').create( 8000 );
 var dexec = require( 'deferred-exec' );
 var dfs = require( '../lib/deferred-fs.js' );
 
-var site = JSON.parse('{"git":"test/gits/original-copy/","deploy":"test/site/deploy/","live":"test/site/live/"}');
+var site = JSON.parse('{"repo":"danheberden/payloads","git":"test/gits/original-copy/","deploy":"test/site/deploy/","live":"test/site/live/","liveBranch":"master","deployOnTag":"true"}');
 
 // make a new site
 var testSite = gdt( site );
 
 // sanity:
 gdt.verbose( false );
+
+var siteVerifiers = {
+  live: function( test, deployedVersion, dontEndTest ) {
+    var folder = deployedVersion ? site.deploy + 'master/' : site.live;
+    var update1 = dfs.readFile( folder + 'master.txt', 'utf8' );
+
+    var update2 = update1.then( function( err, data ) {
+      test.equal( data, 'updated-master\n', 'master.txt should get updated' );
+      return dfs.readFile( folder + 'additional-master.txt', 'utf8' )
+    });
+
+    var update3 = update2.then( function( err, data ) {
+      test.equal( data, 'additional-master\n', 'additional-master.txt should get copied' );
+    }, function(){
+      console.log( 'failed', arguments ); 
+    }).always( function() {
+      if ( !dontEndTest ) {
+        test.done();
+      }
+    });
+  },
+  stage: function( test, folder ) {
+    var update1 = dfs.readFile( folder + 'master.txt', 'utf8' );
+
+    var update2 = update1.then( function( err, data ) {
+      test.equal( data, 'updated-master\n', 'master.txt should get updated' );
+      return dfs.readFile( folder + 'stage.txt', 'utf8' )
+    });
+
+    var update3 = update2.then( function( err, data ) {
+      test.equal( data, 'staging\n', 'stage.txt should get created' );
+      return dfs.readFile( folder + 'additional-master.txt', 'utf8' );
+    });
+
+    var update4 = update3.then( function( err, data ) {
+      test.equal( data, 'additional-master\n', 'additional-master.txt should get copied' );
+    }, function(){
+      console.log( 'failed', arguments ); 
+    }).always( function() {
+      test.done();
+    });
+
+  }
+};
+     
+// failsafe
+setTimeout( function() {
+  process.exit(127);
+}, 10000 );
 
 exports['gdt update stage and master'] = {
   setUp: function(done) {
@@ -51,7 +100,7 @@ exports['gdt update stage and master'] = {
   },
   tearDown: function(done) {
     // clean up behind ourselvesj
-    dexec( 'rm -r test/gits/original-copy; rm -r test/site/live; rm -r test/site/deploy' )
+    dexec( 'rm -rf test/gits/original-copy; rm -rf test/site/live; rm -r test/site/deploy' )
       .then( function() {
         done();
       }, function() {
@@ -59,50 +108,42 @@ exports['gdt update stage and master'] = {
         done();
       });
   },
-  'update master': function(test) {
+  'update master manually': function(test) {
     test.expect( 2 );
     // tests here
-    var update1 = testSite.live( 'master' ).then( function(){
-      return dfs.readFile( site.live + 'master.txt', 'utf8' );
-    });
-
-    var update2 = update1.then( function( err, data ) {
-      test.equal( data, 'updated-master\n', 'master.txt should get updated' );
-      return dfs.readFile( site.live + 'additional-master.txt', 'utf8' )
-    });
-
-    var update3 = update2.then( function( err, data ) {
-      test.equal( data, 'additional-master\n', 'additional-master.txt should get copied' );
-    }, function(){
-      console.log( 'failed', arguments ); 
-    }).always( function() {
-      test.done();
+    testSite.live().then( function(){
+      return siteVerifiers.live( test );
     });
   },
 
-  'update staging' : function(test) {
+  'update staging manually' : function(test) {
     test.expect( 3 );
     var folder = site.deploy + 'stage/';
-    var update1 = testSite.stage( 'stage' ).then( function(){
-      return dfs.readFile( folder + 'master.txt', 'utf8' );
+    testSite.stage( 'stage' ).then( function(){
+      return siteVerifiers.stage( test, folder );
     });
-
-    var update2 = update1.then( function( err, data ) {
-      test.equal( data, 'updated-master\n', 'master.txt should get updated' );
-      return dfs.readFile( folder + 'stage.txt', 'utf8' )
-    });
-
-    var update3 = update2.then( function( err, data ) {
-      test.equal( data, 'staging\n', 'stage.txt should get created' );
-      return dfs.readFile( folder + 'additional-master.txt', 'utf8' );
-    });
-
-    var update4 = update3.then( function( err, data ) {
-      test.equal( data, 'additional-master\n', 'additional-master.txt should get copied' );
-    }, function(){
-      console.log( 'failed', arguments ); 
-    }).always( function() {
+  },
+  'gith server created on default port': function(test) {
+    test.expect(1);
+    testSite.start().then( function( g ) {
+      test.ok( testSite.gith.port, 8000, "gith server should be running on port 8000" );
       test.done();
     });
+  },
+  'test payload - update master with tag' : function(test) {
+    test.expect(4);
+    var json = require( './payloads/update-master-tag.json' );
+
+    // verify the site once gith runs
+    testSite.attr( 'githCallback', function() {
+      siteVerifiers.live( test, false, true );
+      siteVerifiers.live( test, true );
+    });
+
+    // broadcast the payload
+    testSite.start().then( function( g ) {
+      testSite.gith.payload( json );
+    });
   }
+
 };
